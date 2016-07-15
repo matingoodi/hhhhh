190846327b65c50b8925641ecfc347a6521c9811
matingoodi <matingud@gmail.com>
2016-07-15 05:33:18 +0430

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '171312785:AAHIw-JMEkIPZwiJmbry5RNeg_au2zSH_Qw');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
